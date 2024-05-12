43e58fffee4a63318eac95524fbf02fe7d079926
gsarenas <guga_arenas@hotmail.com>
2024-05-12 10:24:20 -0300

    Update setup

--- launch/ld19.launch.py
+++ launch/ld19.launch.py
@@ -31,8 +31,8 @@ def generate_launch_description():
       parameters=[
         {'product_name': 'LDLiDAR_LD19'},
         {'topic_name': 'scan'},
-        {'frame_id': 'base_laser'},
-        {'port_name': '/dev/ttyUSB0'},
+        {'frame_id': 'laser_frame'},
+        {'port_name': '/dev/ttyUSB1'},
         {'port_baudrate': 230400},
         {'laser_scan_dir': True},
         {'enable_angle_crop_func': False},
@@ -45,8 +45,8 @@ def generate_launch_description():
   base_link_to_laser_tf_node = Node(
     package='tf2_ros',
     executable='static_transform_publisher',
-    name='base_link_to_base_laser_ld19',
-    arguments=['0','0','0.18','0','0','0','base_link','base_laser']
+    name='base_link_to_laser_frame_ld19',
+    arguments=['0','0','0','0','0','0','base_link','laser_frame']
   )
 
 
